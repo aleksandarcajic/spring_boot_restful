3f654673f39c58a4347e7f43768758686ae18d4e
Aleksandar Cajic <cajicaleksandar@gmail.com>
2016-10-13 11:03:46 +0200

    Init

---
+++ src/main/java/com/skywalkers/MavenRestSpringApplication.java
@@ -0,0 +1,12 @@
+package com.skywalkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenRestSpringApplication.class, args);
+	}
+}
